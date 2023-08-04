51da14f41fd3aed7c0b549cd775e243dac9f6397
armanizone <kurama.zxc@mail.ru>
2023-08-04 23:54:54 +0600

    save

---
+++ pb_migrations/1690471273_updated_images.js
@@ -0,0 +1,231 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("oatxc3dg14o3k0m")
+
+  // remove
+  collection.schema.removeField("g5il60vt")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "dyayiqwg",
+    "name": "1",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": [],
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "wvagdwex",
+    "name": "2",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": [],
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "zqsoxfsz",
+    "name": "3",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": [],
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "pwrbpmcm",
+    "name": "4",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": [],
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "x80cmt5t",
+    "name": "5",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": [],
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "qxr2mpqj",
+    "name": "6",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": [],
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "noex8g2p",
+    "name": "7",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": [],
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "9jexsjtl",
+    "name": "8",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": [],
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "afskxcql",
+    "name": "9",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": [],
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "whq7dmev",
+    "name": "10",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": [],
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("oatxc3dg14o3k0m")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "g5il60vt",
+    "name": "images",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 99,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": [],
+      "protected": false
+    }
+  }))
+
+  // remove
+  collection.schema.removeField("dyayiqwg")
+
+  // remove
+  collection.schema.removeField("wvagdwex")
+
+  // remove
+  collection.schema.removeField("zqsoxfsz")
+
+  // remove
+  collection.schema.removeField("pwrbpmcm")
+
+  // remove
+  collection.schema.removeField("x80cmt5t")
+
+  // remove
+  collection.schema.removeField("qxr2mpqj")
+
+  // remove
+  collection.schema.removeField("noex8g2p")
+
+  // remove
+  collection.schema.removeField("9jexsjtl")
+
+  // remove
+  collection.schema.removeField("afskxcql")
+
+  // remove
+  collection.schema.removeField("whq7dmev")
+
+  return dao.saveCollection(collection)
+})
