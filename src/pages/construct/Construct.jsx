9c727f730215061277463cba77f21309356fa7c4
armanizone <kurama.zxc@mail.ru>
2023-08-06 15:53:36 +0600

    check

--- src/pages/construct/Construct.jsx
+++ src/pages/construct/Construct.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
+import { Button, Textarea } from '@mantine/core'
+import { pb } from 'shared/api'
+
+async function getQuestions () {
+  return (await pb.collection('questions').getFullList())[0]
+}
 
 export const Construct = () => {
+
+  const [questions, setQuestions] = React.useState({})
+
+  function handleQuestionChange (e) {
+    const { value, name } = e?.currentTarget
+    setQuestions({...questions, [name]: value})
+  }
+
+  async function saveQuestions () {
+    await pb.collection('questions').update(questions?.id, questions)
+  }
+
+  React.useEffect(() => {
+    getQuestions()
+    .then(res => {
+      setQuestions(res)
+    })
+  }, [])
+
   return (
-    <div>Construct</div>
+    <div className='w-full'>
+      <div className='max-w-xs'>
+        {Object.keys(questions).map((key, i) => {
+          if (!isNaN(key)) return (
+            <Textarea
+              label={`Вопрос ${i + 1}`}
+              autosize
+              name={`${i + 1}`}
+              value={questions?.[key]}
+              onChange={handleQuestionChange}
+            />
+          )
+        })}
+
+        <Button
+          onClick={saveQuestions}
+        >
+          Сохранить
+        </Button>
+
+      </div>
+    </div>
   )
 }
