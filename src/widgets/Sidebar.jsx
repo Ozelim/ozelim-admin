3bf820ab143c12fd4650f1bc5325cea227c91ee3
armanizone <kurama.zxc@mail.ru>
2024-05-29 12:54:06 +0500

    tester

--- src/widgets/Sidebar.jsx
+++ src/widgets/Sidebar.jsx
@@ -30,7 +30,7 @@ export const Sidebar = () => {
     {path: '/services', labelru: 'Услуги', labelkz: 'Қызметтер', tur: true},
     {path: '/replenish', labelru: 'Пополнения', labelkz: 'Пополнения'},
     {path: '/bonuses', labelru: 'Бонусы', labelkz: 'Бонусы'},
-    // {path: '/tester', labelru: 'Тестирования', labelkz: 'Тестирования'},
+    {path: '/tester', labelru: 'Тестирования', labelkz: 'Тестирования'},
   ]
 
   const { pathname } = useLocation()
