ce5362873df5e3438e6e4b6f41f223fe3c21a70f
armanizone <kurama.zxc@mail.ru>
2024-12-29 18:26:42 +0500

    r

--- src/pages/withdraws/Withdraws.jsx
+++ src/pages/withdraws/Withdraws.jsx
@@ -303,7 +303,7 @@ export const Withdraws = () => {
             <Tabs.Tab value='ended'>Завершенные</Tabs.Tab>
             <Tabs.Tab value='directors'>Региональные директоры</Tabs.Tab>
           </Tabs.List>
-          <Tabs.Panel value='directors'>
+          {/* <Tabs.Panel value='directors'>
             <div className='grid grid-cols-[15%_auto] min-h-screen'>
               <div className='bg-white border-r shadow-sm'>
                 <div 
@@ -501,8 +501,8 @@ export const Withdraws = () => {
               )}
 
             </div>
-          </Tabs.Panel>
-          {/* <Tabs.Panel value='created' pt='lg'>
+          </Tabs.Panel> */}
+          <Tabs.Panel value='created' pt='lg'>
             <Button onClick={exportToExcel}>Скачать Excel</Button>
             <Table
               striped
@@ -580,7 +580,7 @@ export const Withdraws = () => {
               </tbody>
             </Table>
 
-          </Tabs.Panel>  */}
+          </Tabs.Panel> 
           <Tabs.Panel value='ended'>
             <div className="flex items-end">
               <TextInput
