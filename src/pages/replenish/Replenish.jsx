009f49057c4c8fd85a67374726f72bf3d1af0b4a
armanizone <kurama.zxc@mail.ru>
2025-02-03 18:22:16 +0500

    w

--- src/pages/replenish/Replenish.jsx
+++ src/pages/replenish/Replenish.jsx
@@ -74,44 +74,73 @@ export const Replenish = () => {
           alt="" 
           className='w-[150px] h-[150px] object-cover rounded-full mx-auto mb-5'
         />
-        <ul className='space-y-2'>
-          <li className='grid grid-cols-2'>
-            <p>ID:</p>
-            <p>{userData?.data?.id}</p>
-          </li>
-          <li className='grid grid-cols-2'>
-            <p>Имя:</p>
-            <p>{userData?.data?.name}</p>
-          </li>
-          <li className='grid grid-cols-2'>
-            <p>Фамилия:</p>
-            <p>{userData?.data?.surname}</p>
-          </li>
-          <li className='grid grid-cols-2'>
-            <p>Телефон:</p>
-            <p>{userData?.data?.phone}</p>
-          </li>
-          <li className='grid grid-cols-2'>
-            <p>Область:</p>
-            <p>{userData?.data?.region}</p>
-          </li>
-          <li className='grid grid-cols-2'>
-            <p>Партнеры:</p>
-            <p>{userData?.data?.referals?.length}</p>
-          </li>
-          <li className='grid grid-cols-2'>
-          <p>Бинар:</p>
-            <p>{userData?.data?.bin ? 'Да' : 'Нет'}</p>
-          </li>
-          <li className='grid grid-cols-2'>
-            <p>Уровень:</p>
-            <p>{userData?.data?.level}</p>
-          </li>
-          <li className='grid grid-cols-2'>
-            <p>Дата рег:</p>
-            <p>{dayjs(userData?.data?.created).format('DD.MM.YY')}</p>
-          </li>
-        </ul>
+        <li className='grid grid-cols-2'>
+          <p>ID:</p>
+          <p>{userData?.data?.id}</p>
+        </li>
+        {userData?.data?.user === '' ? (
+          <ul className='space-y-2'>
+            <li className='grid grid-cols-2'>
+              <p>ID:</p>
+              <p>{userData?.data?.id}</p>
+            </li>
+            <li className='grid grid-cols-2'>
+              <p>ФИО:</p>
+              <p>{userData?.data?.fio}</p>
+            </li>
+            <li className='grid grid-cols-2'>
+              <p>Телефон:</p>
+              <p>{userData?.data?.phone}</p>
+            </li>
+            <li className='grid grid-cols-2'>
+              <p>Адрес:</p>
+              <p>{userData?.data?.region}</p>
+            </li>
+            <li className='grid grid-cols-2'>
+              <p>Дата рег:</p>
+              <p>{dayjs(userData?.data?.created).format('DD.MM.YY')}</p>
+            </li>
+          </ul>
+        ) : (
+          <ul className='space-y-2'>
+            <li className='grid grid-cols-2'>
+              <p>ID:</p>
+              <p>{userData?.data?.id}</p>
+            </li>
+            <li className='grid grid-cols-2'>
+              <p>Имя:</p>
+              <p>{userData?.data?.name}</p>
+            </li>
+            <li className='grid grid-cols-2'>
+              <p>Фамилия:</p>
+              <p>{userData?.data?.surname}</p>
+            </li>
+            <li className='grid grid-cols-2'>
+              <p>Телефон:</p>
+              <p>{userData?.data?.phone}</p>
+            </li>
+            <li className='grid grid-cols-2'>
+              <p>Область:</p>
+              <p>{userData?.data?.region}</p>
+            </li>
+            <li className='grid grid-cols-2'>
+              <p>Партнеры:</p>
+              <p>{userData?.data?.referals?.length}</p>
+            </li>
+            <li className='grid grid-cols-2'>
+            <p>Бинар:</p>
+              <p>{userData?.data?.bin ? 'Да' : 'Нет'}</p>
+            </li>
+            <li className='grid grid-cols-2'>
+              <p>Уровень:</p>
+              <p>{userData?.data?.level}</p>
+            </li>
+            <li className='grid grid-cols-2'>
+              <p>Дата рег:</p>
+              <p>{dayjs(userData?.data?.created).format('DD.MM.YY')}</p>
+            </li>
+          </ul>
+        )}
       </Modal>
     </>
   )
